651a7f609c2d6f6232eedc6812c1c10e017bb058
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-02-16 16:25:30 +0100

    Added few repository methods

--- src/main/java/com/example/elasticsearch_tutorial/repo/CarRepository.java
+++ src/main/java/com/example/elasticsearch_tutorial/repo/CarRepository.java
@@ -8,4 +8,10 @@ import java.util.List;
 public interface CarRepository extends ElasticsearchRepository<CarModel, String> {
 
     List<CarModel> findAllByBrand(String brand);
+
+    List<CarModel> findByBrandContaining(String model);
+
+    List<CarModel> findByBrandIn(List<String> brands);
+
+    List<CarModel> saveAll(List<CarModel> cars);
 }
